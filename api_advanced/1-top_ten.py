8c37ddd2ef6ed0bcd5aba10778f467b08f9ce898
fadhuweb <f.abdulazee@alustudent.com>
2024-07-06 20:15:07 +0000

    task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""get hot post function"""
-
-
-import json
+"""" Top Ten Limit"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """get top ten hot post"""
-    if len(sys.argv) < 2:
-        return print(None)
+    """"top ten"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    my_headers = {
+        "User-Agent": "api-advanced/project"
+        }
+
+    response = requests.get(URL, headers=my_headers)
+    # raw_response = response.json()['data']['children']
+
+    if response.status_code != 200:
+        print(None)
     else:
-        url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        listing = []
-        if result.status_code != 200:
-            return print(None)
-        body = json.loads(result.text)
-        for i in body["data"]["children"]:
-            print(i["data"]["title"])
+        json_response = response.json()
+        posts = json_response['data']['children']
+        [print(post.get('data').get('title')) for post in posts]
